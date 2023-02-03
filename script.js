76e5680bb9896ac617a6ba9eba2089f2137aca00
Pablo <pablo.and.ramirez@outlook.com>
2023-02-03 10:06:43 -0300

    clase 05 completada

--- script.js
+++ script.js
@@ -1,56 +1,29 @@
-( () => {
-    
-    const btn = document.querySelector("[data-form-btn]");
-    
-    const createTask = (evento) => {
-        evento.preventDefault();
-        const input = document.querySelector("[data-form-input]");
-        const list = document.querySelector("[data-list]");
-        const value = input.value;
-        const task = document.createElement("li");
-        task.classList.add("card");
-        input.value = "";
-        const taskContent = document.createElement("div");
-        const titleTask = document.createElement("span");
-        titleTask.classList.add("task");
-        titleTask.innerText = value;
-        taskContent.appendChild(checkComplete());
-        taskContent.appendChild(titleTask);
-        //task.innerHTML = content;
-        list.appendChild(task);
-        task.appendChild(taskContent);
-        task.appendChild(deleteIcon());
-    };
-    
-    
-    
-    //Arrow functions o funciones anonimas
-    btn.addEventListener("click",  createTask);
-    
-    const checkComplete = () => {
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i;
-    };
-    
-    //Inmediate invoked function expression IIFE
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle("fas");
-        element.classList.toggle("completeIcon");
-        element.classList.toggle("far");
-    }
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-    const deleteIcon = () =>{
-        const i = document.createElement("i");
-        i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
-        i.addEventListener("click", deleteTask);
-        return i;
-    }
+const btn = document.querySelector("[data-form-btn]");
+    
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const list = document.querySelector("[data-list]");
+    const value = input.value;
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    //task.innerHTML = content;
+    list.appendChild(task);
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
+};
 
-    const deleteTask = (event) => {
-        const parent = event.target.parentElement;
-        parent.remove();
-    }
-    })();
+
+
+//Arrow functions o funciones anonimas
+btn.addEventListener("click",  createTask);
