bdaecb2fd0035bf740e15541eebbd0089becdb8c
Pablo <pablo.and.ramirez@outlook.com>
2023-02-03 09:53:42 -0300

    clase 05 completada

--- script.js
+++ script.js
@@ -10,19 +10,17 @@
         const task = document.createElement("li");
         task.classList.add("card");
         input.value = "";
-        //backticks
         const taskContent = document.createElement("div");
         const titleTask = document.createElement("span");
         titleTask.classList.add("task");
         titleTask.innerText = value;
         taskContent.appendChild(checkComplete());
         taskContent.appendChild(titleTask);
-        const content = `
-            <i class="fas fa-trash-alt trashIcon icon"></i>`;
         //task.innerHTML = content;
         list.appendChild(task);
         task.appendChild(taskContent);
-    }
+        task.appendChild(deleteIcon());
+    };
     
     
     
@@ -34,7 +32,7 @@
         i.classList.add("far", "fa-check-square", "icon");
         i.addEventListener("click", completeTask);
         return i;
-    }
+    };
     
     //Inmediate invoked function expression IIFE
     const completeTask = (event) => {
@@ -43,5 +41,17 @@
         element.classList.toggle("completeIcon");
         element.classList.toggle("far");
     }
-    
+
+    const deleteIcon = () =>{
+        const i = document.createElement("i");
+        i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+        i.addEventListener("click", deleteTask);
+        return i;
+    }
+
+    const deleteTask = (event) => {
+        const parent = event.target.parentElement;
+        parent.remove();
+    }
+
     })();
